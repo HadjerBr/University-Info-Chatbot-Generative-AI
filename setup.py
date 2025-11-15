fb92ace32c268c6a5b0eca29ced3ac69f461a992
HadjerBr <hadjerbrioua@gmail.com>
2025-11-15 14:38:27 +0300

    Adding folder structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="CampusMate-University-Info-Chatbot",
+    version="0.1.0",
+    author="Hadjer Brioua",
+    author_email="hadjerbrioua@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
